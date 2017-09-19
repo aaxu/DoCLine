c4969d2ac9cc2319baa424b924e6517976138cf3
Aaron Xu <aaron_rg_xu@hotmail.com>
2017-09-19 08:59:48 -0700

    Using subprocess module to call pydoc and get the output

--- docline.py
+++ docline.py
@@ -1,7 +1,5 @@
 import sys
-import urllib
-import requests
-from bs4 import BeautifulSoup
+import subprocess
 from reppy.robots import Robots
 
 USER_AGENT = 'DoCLine Parser (http://github.com/aaxu/docline)'
@@ -19,23 +17,10 @@ def check_website_policy(url):
     return robot.allowed(url, USER_AGENT)
 
 def main():
-    doc_website = 'http://docs.python.org/2/'
-    doc_search_url = 'https://docs.python.org/2/search.html?q='
-    if check_website_policy(doc_website):
-        if len(sys.argv) > 1:
-            args = sys.argv[1:]
-            query = " ".join(args)
-            encoded_query = urllib.quote_plus(query)
-            search_url = doc_search_url + encoded_query
-            search_res = requests.get(search_url)
-            soup = BeautifulSoup(search_res.text, 'html.parser')
-            print search_url
-            print search_res
-            print soup
-        else:
-            exit(0)
-    else:
-        print "This website doesn't allow scraping."
+    command = sys.argv
+    sys.argv[0] = 'pydoc'
+    process = subprocess.Popen(command, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+    print process.communicate()
 
 if __name__ == '__main__':
     main()
