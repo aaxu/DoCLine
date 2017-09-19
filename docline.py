f90ee7a2a6e5dd6a8f80bd1190d69afce2f9f4e4
Aaron Xu <aaron_rg_xu@hotmail.com>
2017-09-19 13:39:47 -0700

    Wrapped around lines can now be indented

--- docline.py
+++ docline.py
@@ -22,31 +22,37 @@ def check_website_policy(url):
     robot = Robots.fetch(robot_url)
     return robot.allowed(url, USER_AGENT)
 
-def format_line(line):
+def format_line(line, line_indentation):
     """
     Takes in a string and returns a list of strings such that they
     fit nicely on the terminal screen.
     Args:
         line: The line you want to format.
+        line_indentation: The string indentation that a line should
+                          take when the line wraps around the screen.
 
     Returns:
         A list of strings that you can print consecutively to output
         a nicely formatted text.
     """
     output = []
+    margin_right = 3
     formatted_line = line.strip()
-    if len(formatted_line) <= COLUMNS:
+    if formatted_line == '' or formatted_line == None:
+        return []
+    formatted_line = line_indentation + formatted_line
+    if len(formatted_line) <= COLUMNS - margin_right:
         return [formatted_line]
-    break_line_at_index = COLUMNS
+    break_line_at_index = COLUMNS - margin_right
     while (not formatted_line[break_line_at_index].isspace() and
-           break_line_at_index > 0):
+           break_line_at_index >= len(line_indentation)):
         break_line_at_index -= 1
-    if break_line_at_index > 0:
-        output.append(formatted_line[0:break_line_at_index])
-        leftover_string = INDENTATION + formatted_line[break_line_at_index:]
-        output.extend(format_line(leftover_string))
-    else:
-        output.append(formatted_line)
+    # Case for one really long word
+    if break_line_at_index < len(line_indentation):
+        break_line_at_index = COLUMNS - margin_right
+    output.append(formatted_line[:break_line_at_index])
+    leftover_string = formatted_line[break_line_at_index:]
+    output.extend(format_line(leftover_string, INDENTATION))
     return output
 
 
@@ -57,10 +63,11 @@ def main():
     output = subprocess.check_output(command).split('\n')
     formatted_text = []
     for line in output:
-        formatted_text.extend(format_line(line))
+        formatted_text.extend(format_line(line, ""))
 
-    # output = re.sub(r'[|]', ' ', output)
-    print "\n".join(formatted_text)
+    formatted_text = "\n".join(formatted_text)
+    formatted_text = re.sub(r'[|]', '  ', formatted_text)
+    print formatted_text
 
 if __name__ == '__main__':
     main()
