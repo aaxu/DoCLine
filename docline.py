f58cd88b01d32d47dff56adc9cb63f5711e1f64b
Aaron Xu <aaron_rg_xu@hotmail.com>
2017-09-21 14:46:31 -0700

    Added blue text for constants and changed regex for matching function heads

--- docline.py
+++ docline.py
@@ -23,6 +23,16 @@ def check_website_policy(url):
     robot = Robots.fetch(robot_url)
     return robot.allowed(url, USER_AGENT)
 
+def blue_text(text):
+    """
+    Args:
+        text: The string you want to print in blue.
+
+    Returns:
+        The same string, but will be blue when printed.
+    """
+    return color_text(text, colorama.Fore.BLUE)
+
 def yellow_text(text):
     """
     Args:
@@ -45,8 +55,16 @@ def color_text(text, color):
     """
     return color + text + colorama.Style.RESET_ALL
 
+def format_text_colors(text):
+    # Make the header of each function yellow.
+    # This regex matches anything of the forms
+    # func_name(...) or func_name(self) or func_name(self, ...)
+    text = re.sub(r'(\w+\((?:\.{3}|self.*)\))', yellow_text(r'\1'), text)
+    # Make all constant names blue.
+    text = re.sub(r'(\s[A-Z0-9_\-]*\s=)', blue_text(r'\1'), text)
+    return text
 
-def format_line(line):
+def format_line_wrap(line):
     """
     Args:
         line: The line you want to format.
@@ -105,13 +123,13 @@ def main():
     command = sys.argv
     sys.argv[0] = 'pydoc'
     output = subprocess.check_output(command)
-    output = re.sub(r'\|', '  ', output)
+    output = re.sub(r'\|', r' ', output)
     lines = output.split('\n')
     formatted_text = []
     for line in lines:
-        formatted_text.extend(format_line(line))
+        formatted_text.extend(format_line_wrap(line))
     output = "\n".join(formatted_text)
-    output = re.sub(r'(.+\(\.\.\.\))', yellow_text(r'\1'), output)
+    output = format_text_colors(output)
     print '\n', output
 
 if __name__ == '__main__':
